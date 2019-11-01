38c971050ae6d05e03c72843002ae3f741ea1e94
syu-y <syu12580526@gmail.com>
2019-11-01 15:11:21 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+// import WebView from 'react-native-webview';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <WebView
+          onLayout={this.reRenderWebView.bind(this)}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          scalesPageToFit={true}
+          scrollEnabled={false}
+          automaticallyAdjustContentInsets={true}
+          {...this.props}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
